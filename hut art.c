62eab9736f60f63dd262118430a141c986272bd5
Shuvra Saha Rimjim <110848482+shuvra789@users.noreply.github.com>
2022-08-19 01:15:59 +0600

    Add files via upload

---
+++ hut art.c
@@ -0,0 +1,27 @@
+//Write a function to draw a rocket.
+#include <stdio.h>
+
+ void rocket(void);
+
+ int main(void)
+ {
+     rocket();
+     return 0;
+ }
+
+  void rocket(void)
+  {
+      printf("        __________________________________       \n");
+      printf("       /  \\                              \\     \n");
+      printf("      /    \\                              \\    \n");
+      printf("     /______\\______________________________\\   \n");
+      printf("    |        |                              |    \n");
+      printf("    |        |                     __       |    \n");
+      printf("    |        |          ______    |  |      |    \n");
+      printf("    |        |         |      |   |__|      |    \n");
+      printf("    |        |         |      |             |    \n");
+      printf("    |        |         |      |             |    \n");
+      printf("    |________|_________|______|_____________|    \n");
+
+  }
+
